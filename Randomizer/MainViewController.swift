08dc2c879f838c1d8e23b0467c42da6bd3045a7a
Kseniya Barneva <kseniya.barneva@mail.ru>
2021-09-05 08:43:21 +0300

    added button Save

--- Randomizer/MainViewController.swift
+++ Randomizer/MainViewController.swift
@@ -18,11 +18,25 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         getRandomNumberButton.layer.cornerRadius = 15
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else {
+            return
+        }
+        settingsVC.minValue = minimumValueLabel.text
+        settingsVC.maxValue = maximumValueLabel.text
+    }
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        guard let settingsVC = segue.source as? SettingsViewController else {
+            return }
+        minimumValueLabel.text = settingsVC.minimumTextField.text
+        maximumValueLabel.text = settingsVC.maximumTextField.text
+    }
     
     @IBAction func getRandomNumberButtonTapped() {
         let minimunNumber = Int(String(minimumValueLabel.text ?? "")) ?? 0
         let maximumNumber = Int(String(maximumValueLabel.text ?? "")) ?? 100
         randomValueLabel.text = String(Int.random(in:minimunNumber...maximumNumber))
     }
+    
 }
 
--- Randomizer/SettingsViewController.swift
+++ Randomizer/SettingsViewController.swift
@@ -11,8 +11,14 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var minimumTextField: UITextField!
     @IBOutlet weak var maximumTextField: UITextField!
     
+    var minValue: String!
+    var maxValue: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        minimumTextField.text = minValue
+        maximumTextField.text = maxValue
     }
     @IBAction func tappedCancelButton() {
         dismiss(animated: true)
